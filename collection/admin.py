47e137599ae0623add2884601cc551d4af0d170f
xXDuckXx <dacs2010@gmail.com>
2018-11-15 13:04:53 -0500

    better css

--- collection/admin.py
+++ collection/admin.py
@@ -5,7 +5,10 @@ from collection.models import Thing
 
 class ThingAdmin(admin.ModelAdmin):
     model = Thing
-    list_display = ('name','description',)
+    list_display = (
+        'name',
+        'description',
+    )
     prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Thing, ThingAdmin)
--- collection/models.py
+++ collection/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Thing(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, null=False)
     description = models.TextField()
-    # image = models.ImageField(upload_to="", default="static/thing_images/deafault")
+    image = models.ImageField(upload_to="", default="/static/images/thing_images/default/default_cabbage.jpg")
     slug = models.SlugField(unique=True)
