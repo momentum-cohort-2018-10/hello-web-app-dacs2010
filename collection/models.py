9b15ac19864bd59e0f4704ed9bd4e03edeebb29f
xXDuckXx <dacs2010@gmail.com>
2018-11-15 09:56:08 -0500

    css changes

--- collection/models.py
+++ collection/models.py
@@ -4,4 +4,5 @@ from django.db import models
 class Thing(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+    # image = models.ImageField(upload_to="", default="static/thing_images/deafault")
     slug = models.SlugField(unique=True)
