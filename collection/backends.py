54f7fa8925352289a093b2b2672a28fbc042a271
xXDuckXx <dacs2010@gmail.com>
2018-11-19 14:24:16 -0500

    registration to create object working

--- collection/backends.py
+++ collection/backends.py
@@ -1,11 +1,6 @@
-from registration.views import RegistrationView
+from registration.backends.simple.views import RegistrationView
 
 
 # so this is a subclass, gonna rewrite portion
 class MyRegistrationView(RegistrationView):
     success_url = 'registration_create_thing'
-
-
-# class MyRegistrationView(RegistrationView):
-#     def get_success_url(user):
-#         return success_url = 'registration_create_thing'
--- hellowebapp/urls.py
+++ hellowebapp/urls.py
@@ -33,36 +33,30 @@ from django.views.generic import TemplateView
 from collection import views
 
 
-
-
-
 urlpatterns = [
-     path('', views.index, name='home'),
-     path('about/',
-          TemplateView.as_view(
-               template_name='about.html'
-          ),
-          name='about'),
-     path('contact/',
-          TemplateView.as_view(
-               template_name='contact.html'
-          ),
-          name='contact'),
-     path('things/<slug>/',
-          views.thing_detail,
-          name='thing_detail'),
-     path('things/<slug>/edit/',
-          views.edit_thing, name='edit_thing'),
+     # objects to users
+     path('accounts/register/',
+          MyRegistrationView.as_view(),
+          name='registration_register'),
+     path('accounts/create_thing/',
+          views.create_thing,
+          name='registration_create_thing'),
+
+     # registration
+     path('accounts/', include('registration.backends.simple.urls')),
+
+     # admin
+     path('admin/', admin.site.urls),
 
      # password reset
      path('accounts/password/reset/',
           password_reset,
           {'template_name':
-          'registration/password_reset_form.html'},name="password_reset"),
+          'registration/password_reset_form.html'}, name="password_reset"),
      path('accounts/password/reset/done/',
           password_reset_done,
           {'template_name':
-          'registration/password_reset_done.html'},name="password_reset_done"),
+          'registration/password_reset_done.html'}, name="password_reset_done"),
      path('accounts/password/reset/<uidb64>/<token>/',
           password_reset_confirm,
           {'template_name':
@@ -74,19 +68,25 @@ urlpatterns = [
           'registration/password_reset_complete.html'},
           name="password_reset_complete"),
 
-     # registration
-     path('accounts/', include('registration.backends.simple.urls')),
+     # slug addresses
+     path('things/<slug>/',
+          views.thing_detail,
+          name='thing_detail'),
+     path('things/<slug>/edit/',
+          views.edit_thing, name='edit_thing'),
 
-     # admin
-     path('admin/', admin.site.urls),
-
-     # objects to users
-     path('accounts/register/', 
-          MyRegistrationView.as_view(),
-          name='registration_register'),
-     path('accounts/create_thing/', 
-          views.create_thing,
-          name='registration_create_thing'),
+     # main pages
+     path('', views.index, name='home'),
+     path('about/',
+          TemplateView.as_view(
+               template_name='about.html'
+          ),
+          name='about'),
+     path('contact/',
+          TemplateView.as_view(
+               template_name='contact.html'
+          ),
+          name='contact'),
 ]
 
 # if settings.DEBUG:
